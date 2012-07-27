6a624be04eb98905c4070baccad5f5ce0c4c1768
LittleAcer <hnaik@pclip.ltd.uk>
2012-07-27 11:08:58 +0100

    Mrs Hemangini Naik

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace RFID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.snap = new System.Windows.Forms.PictureBox();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.snap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // snap
+            // 
+            this.snap.Location = new System.Drawing.Point(98, 33);
+            this.snap.Name = "snap";
+            this.snap.Size = new System.Drawing.Size(152, 150);
+            this.snap.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.snap.TabIndex = 1;
+            this.snap.TabStop = false;
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Location = new System.Drawing.Point(152, 205);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(0, 13);
+            this.lblFullName.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Worked Time :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(101, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 253);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblFullName);
+            this.Controls.Add(this.snap);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.snap)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox snap;
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+    using System.Data.SqlClient;
+
+    namespace RFID
+    {
+        public partial class Form1 : Form
+        {
+            SqlDataReader rdr = null;
+            SqlConnection con = null;
+            SqlCommand cmd = null;
+            string ConnectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=D:\\RFID_Employee_In_Out\\RFID\\RFID\\RFID.mdf;Integrated Security=True;User Instance=True";
+          
+            public Form1()
+            {
+                InitializeComponent();
+               
+            }
+
+            private void textBox1_TextChanged(object sender, EventArgs e)
+            {
+
+                if (timer1.Enabled)
+                {
+                    MessageBox.Show("You are swapping more than once" + timer1.Tag);                    
+                }
+                if (textBox1.Text.ToString().Length == 8)
+                {                
+                    try
+                    {                    
+                        con = new SqlConnection(ConnectionString);
+                        con.Open();
+                                      
+                        string CommandText = "SELECT first_name, last_name , snap" +
+                                             "  FROM tblContact" +
+                                             " Where ID_number = '" + textBox1.Text.ToString()+"'";
+                        cmd = new SqlCommand(CommandText);
+                        cmd.Connection = con;
+
+                        rdr = cmd.ExecuteReader();
+                                                
+                            bool success = false;
+
+                            while (rdr.Read())
+                            {
+                                success = true;
+                                snap.Image = new Bitmap(rdr["snap"].ToString());
+                                lblFullName.Text = rdr["first_name"].ToString() + " " + rdr["last_name"].ToString();                                
+                                textBox1.Hide();
+                                snap.Show();
+                                lblFullName.Show();
+                                label1.Show();
+                                label2.Show();
+                                label3.Show();
+                            }
+
+                            if (success == false)
+                            {
+                                MessageBox.Show("Wrong card entry....");
+                            }
+                            else
+                            {
+
+                                In_Out_Method(textBox1.Text.ToString());
+                                //Calculate number of hours worked
+
+                                con = new SqlConnection(ConnectionString);
+                                con.Open();
+
+                                CommandText = "SELECT in_out , ID_number , in_Time" +
+                                                     "  FROM tblInOut" +
+                                                     " Where ID_number = '" + textBox1.Text.ToString() + "'";
+                                cmd = new SqlCommand(CommandText);
+                                cmd.Connection = con;
+
+                                rdr = cmd.ExecuteReader();
+                                int count = 0;
+                                TimeSpan hours_worked = new TimeSpan();
+                                TimeSpan dt = new TimeSpan();
+                                while (rdr.Read())
+                                {
+                                    if (count == 0)
+                                    {
+                                        count = 1;
+                                        dt = (TimeSpan)rdr["in_Time"];
+                                    }
+                                    else
+                                    {
+                                        count = 0;
+
+                                        hours_worked = hours_worked + dt.Subtract((TimeSpan)rdr["in_Time"]);
+
+                                        //MessageBox.Show(hours_worked.ToString());
+
+                                    }
+                                }
+                                label2.Text = hours_worked.ToString();
+                            }
+
+                                                
+                    }
+                    catch (Exception ex)
+                    {
+                        // Print error message
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally
+                    {
+                       // Close data reader object and database connection
+                        textBox1.Clear();  
+                        if (rdr != null)
+                            rdr.Close();
+
+                        if (con.State == ConnectionState.Open)
+                            con.Close();
+                        timer1.Enabled = true;
+                        
+                        timer1.Start();                        
+                    }
+                
+                }           
+            }
+
+            
+            private bool Is_Signed_Today_Method(String RFID)
+             {
+                bool success = false;
+                try
+                {
+                    con = new SqlConnection(ConnectionString);
+                                       
+                    con.Open();
+
+                    SqlCommand cmd = new SqlCommand("Is_Signed_today", con);
+                  
+                    cmd.Connection = con;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add(new SqlParameter("@IN_OUT", 1));
+                    cmd.Parameters.Add(new SqlParameter("@RFID", RFID.ToString()));
+                 
+                    rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        {
+                            success = true;                    
+                        }                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(""+ex);
+                }
+                
+                return success;
+            }
+            private bool Is_In_Method(String RFID)
+            {
+                bool success = false;
+                try
+                {
+                    con = new SqlConnection(ConnectionString);
+
+                    con.Open();
+
+                    SqlCommand cmd = new SqlCommand("Is_In", con);
+
+                    cmd.Connection = con;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add(new SqlParameter("@RFID", RFID.ToString()));
+
+                    rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        if ((int)rdr["in_out"] == 1)
+                        {
+                            success = true;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("" + ex);
+                }
+                finally
+                {
+                    
+                }
+
+                return success;
+            }
+            private void In_Out_Method(String RFID_number)
+            {
+                String Insert = null;               
+                DateTime datetime = (DateTime)DateTime.Now;
+
+                if (Is_Signed_Today_Method(RFID_number))
+                {
+                    if (Is_In_Method(RFID_number))
+                    {
+                        Insert = "insert into tblInOut(ID_number, in_out, in_Date , in_Time)values ('" + RFID_number + "','" + 0 + "','" + datetime.ToString("yyyy/MM/dd") + "','" + datetime.ToString("HH:mm:ss") + "')";
+                        label3.Text = "Signed out Successfully";
+                    }
+                    else
+                    {
+                        Insert = "insert into tblInOut(ID_number, in_out, in_Date, in_Time)values ('" + RFID_number + "','" + 1 + "','" + datetime.ToString("yyyy/MM/dd") + "','" + datetime.ToString("HH:mm:ss") + "')";
+                        label3.Text = "Signed In Successfully";
+                    }
+                }
+                else
+                {
+                    label3.Text = "Signed In Successfully";
+                    Insert = "insert into tblInOut(ID_number, in_out, in_Date, in_Time)values ('" + RFID_number + "','" + 1 + "','" + datetime.ToString("yyyy/MM/dd") + "','" + datetime.ToString("HH:mm:ss") + "')";
+                }
+
+                try
+                {                   
+                    SqlConnection conn = new SqlConnection(ConnectionString);
+                    conn.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(Insert, conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    conn.Close();
+                }                
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                                
+            }
+        
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                snap.Hide();
+                lblFullName.Hide();
+                label1.Hide();
+                label2.Hide();
+                label3.Hide();
+                timer1.Enabled = false;
+                timer1.Interval = 10000;
+            }
+
+            private void timer1_Tick(object sender, EventArgs e)
+            {
+                snap.Hide();
+                lblFullName.Hide();
+                label1.Hide();
+                label2.Hide();
+                label3.Hide();
+                timer1.Enabled = false;
+                timer1.Interval = 5000;
+                textBox1.Clear();
+                textBox1.Show();
+                textBox1.Focus();
+            }
+
+        }
+    }
